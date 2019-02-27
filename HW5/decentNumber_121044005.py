ebf89c37553aef5dd719a88255e5a014e3d1b029
Kadir Bulut <kadirbulut@users.noreply.github.com>
2019-02-27 14:37:02 +0300

    Update decentNumber_121044005.py

--- HW5/decentNumber_121044005.py
+++ HW5/decentNumber_121044005.py
@@ -1,23 +1,3 @@
-#   Soruda greedy aproach (aç gözlü yaklaşım) kullanmamız istendi.
-#   Verilen digit sayısı büyüklüğündeki sayıyı 3 ve 5 digitleriyle elde edip 
-#   daha doğrusu 3 ve 5 digitleriyle elde edilebilecek sayıların en büyüğü 
-#   sorulduğundan dolayı algoritma gereği ; 
-#   Öncelikle sayıyı tamamen 5 digitleriyle doldurmaya çalışıyorum,bunun için
-#   yani 5 digitleriyle doldurmak için sayının 3 ün katları olma durumunu kontrol    
-#   ediyorum.Bulunabilecek max. sayı tamamen 5 rakamlarıyla elde edilecek sayı.
-#   Bunun için sayının 3 ün tam katı olması gerekli . Eğer değilse her seferinde
-#   3 ün katı olma durumlarını eksiltip 5 in katı olma durumunu 1 artırıp ihtimale 
-#   koyuyorum. Böyleliklede sayının tamamını 5 rakamlarından oluşturamasamda
-#   5 rakamanın içerisinde max. sayıda geçtiği sayıyı elde etmeye çalışıyorum.
-#   İçerisindeki 5 rakamlarının sayısını 3 ün , 3 rakamlarının sayısınıda 5 in
-#   katı olması durumuyla kontrol ederken ; öncelik olarak verdiğim 3 ün katını 
-#   bir azaltıp 5 in katı olma durumlarını devreye sokup sayıyı 3 rakamlarıyla
-#   bulmaya çalışıyorum.Bunu yaparkende elimde olan countlarla bir sayı edip
-#   verilen sayıdan çıkarıp kalanla kontrol ediyorum.Fonksiyonumda 3ün ve 5in 
-#   katları sayısını tutup sonuç olarak ise öncelikle 3 ün sonra 5 in countlarıyla 
-#   bir string oluşturup return ediyorum.
-
-
 def decentNumber(num):
     
     count3=num//3 # first find out how many 3's (for 5 digit)
