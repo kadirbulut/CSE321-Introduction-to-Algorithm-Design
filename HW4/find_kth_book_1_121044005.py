68a520dc3618f0e9aa3189e5dd4511f785801a13
Kadir Bulut <kadirbulut@users.noreply.github.com>
2019-02-27 14:35:47 +0300

    Update find_kth_book_1_121044005.py

--- HW4/find_kth_book_1_121044005.py
+++ HW4/find_kth_book_1_121044005.py
@@ -1,28 +1,3 @@
-#	*Bu soruda logm+logn karmaşıklığı istendiğinden dolayı her aşamada 
-#	m yada n listesinin sol yada sağ yarını alarak k. elemanı bulmaya çalıştım
-#	*Her aşamada listeden attığım eleman sayısı kadar k ya yakınlaştığımı da
-#	kontrol edip k. elemana ulaşmaya çalıştım.
-#	*Öncelikle m yada n listelerinden birinin boşaldığı durumda aranan elemanı
-#	dolu olan listede aradım.(Yani arranan elemanın dolu olan listede olduğu 
-#	kesinleşmiş oluyordu.)
-#	*Yine her aşamada listelerin yarısının uzunluklarının toplamını bularak ve k
-#	ile karşılaştırarak aradığım elemanın soldaki yarım listelerde olup 
-#	olmadığını kontrol ettim.
-#		Kontrol uzunluğumun k dan daha büyük ise aramaya devam ettim listeleri 
-#		küçülterek.
-#		
-#	*Listeleri elemek için ise şu durumları kontrol ettim ; 
-#	-> Eğer sol yarımlarda ise aranan eleman ; 
-#		-Orta elemanları karşılaştırıp , sıralama olarak önde olan elemanın   
-#		bulunduğu listenin sol yarısını alıp sağ yarısını listeden çıkardım her seferinde
-#	-> Eğer aranan eleman sağ yarımlarda ise;
-#		-Yine ortadaki elemanları karşılaştırıp sıralama olarak geride olan
-#		elemanın bulunduğu listenin sağ yarısını alıp sol yarısını listeden
-#		atarak devam ettim.
-#
-#	Gerekli küçültme işlemlerinden sonra kth elemanı return ettim.	
-#
-
 def helper(m,n,k):
     if not m : # if m is null k th element in the n
         return n[k] 
